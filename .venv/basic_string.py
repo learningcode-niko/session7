70005979e2d7b4359d0dc7166b5a797f30aea1e1
learningcodeniko <6nicholasp6@gmail.com>
2026-02-03 17:16:10 +0100

    string examples

---
+++ .venv/basic_string.py
@@ -0,0 +1,17 @@
+s1 = "Hello World"
+s2 = 'Hello World'
+print(s1)
+print(s2)
+s1 = "Fjhfenkjns@#$%^&dkjnee"
+s2 = "ü"
+print(s1)
+print(s2)
+
+# we can index a string
+s = "Banana"
+print(s[0])
+print(s[-1])
+print(s[10//2])
+# len function will tell you the amount of characters
+print(s, len(s))
+
---
+++ string_operators.py
@@ -0,0 +1,11 @@
+# + will perform string concatenation
+s1 = "hello"
+s2 = "bye"
+print(s1+s2)
+print(s2+s1)
+print(s1+" "+s2)
+# you can multiply a string by an integer
+print(3 * s2) # nsync song
+print(s1+" "*10+s2)
+
+
